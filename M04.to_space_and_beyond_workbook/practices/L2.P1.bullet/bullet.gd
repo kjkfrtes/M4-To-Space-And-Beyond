e14139673422d77edb48bf476075df29272b92ba
kjkfrtes <islee2@s.sfusd.edu>
2025-10-23 11:57:11 -0700

    2

--- M04.to_space_and_beyond_workbook/practices/L2.P1.bullet/bullet.gd
+++ M04.to_space_and_beyond_workbook/practices/L2.P1.bullet/bullet.gd
@@ -5,7 +5,7 @@ var velocity := Vector2(500, -1)
 
 # Complete the process function to make the bullet move based on the velocity
 # vector. Also, rotate the bullet to match the velocity vector's angle.
-func _process(_delta float) -> void:
-	velocity += velocity*delta
+func _process(_delta: float) -> void:
+	position += velocity*delta
 	rotate = velocity.angle()
 	pass
