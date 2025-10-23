0ff079ce961693f4c9f9c9cd6562d18e34e34a3f
kjkfrtes <islee2@s.sfusd.edu>
2025-10-23 11:54:31 -0700

    4

--- M04.to_space_and_beyond_workbook/practices/L2.P1.bullet/bullet.gd
+++ M04.to_space_and_beyond_workbook/practices/L2.P1.bullet/bullet.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
 # Change the bullet's velocity to move towards the top right.
-var velocity := Vector2(500*delta, -1*delta)
+var velocity := Vector2(500, -1)
 
 # Complete the process function to make the bullet move based on the velocity
 # vector. Also, rotate the bullet to match the velocity vector's angle.
-func _process(_delta.float) -> void:
+func _process(_delta float) -> void:
+	velocity += velocity*delta
+	rotate = velocity.angle()
 	pass
